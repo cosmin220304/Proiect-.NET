6e9da9f06e9b999f12d77db959ed370da8ecdaf8
daniel-bicu <danielyoi1233@gmail.com>
2020-12-06 21:44:38 +0200

    added base api

---
+++ TweetMe.API/TweetMe.API/Controllers/Tesst.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetMe.API.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class Tesst : ControllerBase
+    {
+
+        [HttpGet]
+        public string Salut()
+        {
+            return "Hello!";
+        }
+    }
+}
