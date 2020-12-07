998cab3113ee462f224ce229c365a32a76ca9b3c
Mititelu Cristina <61682864+Cristina-m-29@users.noreply.github.com>
2020-12-07 23:13:59 +0200

    search-init

--- TweetMeFront/Pages/Search.razor.cs
+++ TweetMeFront/Pages/Search.razor.cs
@@ -1,7 +1,21 @@
-﻿
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+
 namespace TweetMeFront.Pages
 {
     partial class Search
     {
+        private string search = "";
+        private string oldSearch = "";
+        public void initSearch()
+        {
+            search = "";
+        }
+        public void startSearch()
+        {
+            Console.WriteLine(search);
+            oldSearch = search;
+            search = "";
+        }
     }
 }
