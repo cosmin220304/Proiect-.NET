6e31d1343c066d096fd27b547412a43fa892c4b5
daniel-bicu <danielyoi1233@gmail.com>
2020-12-07 23:56:49 +0200

    Stalker started

---
+++ TweetMe.API/Stalker/Controllers/TweetStalker.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Stalker.Controllers
+{
+    [Route("stalker")]
+    [ApiController]
+    public class TweetStalker : ControllerBase
+    {
+        static readonly HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+        [HttpGet("friends/{username}")]
+        public async Task<string> GetFriendsAsync(string username)
+        {
+            string url = $"https://api.twitter.com/1.1/friends/list.json?screen_name={username}";
+         
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+
+            return $"https://api.twitter.com/1.1/friends/list.json?screen_name={username}";
+        }
+    }
+}
