5d1fa8bbf40d56e71feb09dde16fb55ddb4a1276
Cosmin <cosmin.aftanase@yahoo.com>
2020-12-08 02:20:57 +0200

    .

---
+++ TweetMe.API/Tweet-Reco/Controllers/SentimentAnalysisController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using SentimentAnalysisAPI.DataModels;
+using System;
+
+namespace SentimentAnalysisAPI.Controllers
+{
+    [Route("api/v1/predictions")]
+    [ApiController]
+    public class SentimentAnalysisController : ControllerBase
+    {
+        private const string P = "Positive";
+        private const string N = "Negative";
+        private readonly PredictionEnginePool<SentimentData, SentimentPrediction> _predictionEnginePool;
+
+        public SentimentAnalysisController(PredictionEnginePool<SentimentData, SentimentPrediction> predictionEnginePool) 
+        {
+            this._predictionEnginePool = predictionEnginePool;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] SentimentData data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            SentimentPrediction predictedValue = _predictionEnginePool.Predict(modelName: "SentimentAnalysisModel", example: data);
+
+            string sentiment = Convert.ToBoolean(predictedValue.Prediction) ? P : N;
+
+            return Ok(sentiment);
+        }
+    }
+}
---
+++ TweetMe.API/Tweet-Reco/DataModels/SentimentData.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML.Data;
+
+namespace SentimentAnalysisAPI.DataModels
+{
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public string SentimentText { get; set; }
+
+        [LoadColumn(1)]
+        [LoadColumnName("Label")]
+        public bool Sentiment { get; set; }
+    }
+}
