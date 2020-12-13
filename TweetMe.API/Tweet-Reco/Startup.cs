e26ce2e63d84e18df8dde93c4a96f454e97cbbcd
Cosmin <cosmin.aftanase@yahoo.com>
2020-12-13 19:39:35 +0200

    .

--- TweetMe.API/Tweet-Reco/Startup.cs
+++ TweetMe.API/Tweet-Reco/Startup.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.ML;
 using Microsoft.OpenApi.Models;
 using SentimentAnalysisAPI.DataModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SentimentAnalysisAPI
 {
