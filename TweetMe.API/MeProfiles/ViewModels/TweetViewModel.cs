482656d478de696546487fcc3339ef27e8cf72cd
cosmin.aftanase <cosmin.aftanase@expertnetwork.ro>
2020-12-14 19:30:53 +0200

    .

---
+++ TweetMe.API/MeProfiles/ViewModels/TweetViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MeProfiler.ViewModels
+{
+    public class TweetViewModel
+    {
+        public long Id { get; set; }
+
+        public string Text { get; set; }
+        
+        public string Data { get; set; }
+        
+        public int Sentiment { get; set; }
+    }
+}
