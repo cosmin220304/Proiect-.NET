7046f3820a7567a098a1996de44290fa2f8b5942
Mititelu Cristina <61682864+Cristina-m-29@users.noreply.github.com>
2020-12-14 19:38:10 +0200

    search-box

--- TweetMeFront/Pages/Search.razor.cs
+++ TweetMeFront/Pages/Search.razor.cs
@@ -1,21 +1,35 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
-using System;
+﻿using System;
+using System.Net.Http.Json;
 
 namespace TweetMeFront.Pages
 {
     partial class Search
     {
         private string search = "";
-        private string oldSearch = "";
+        private Person person;
+        private string apiUrl = "https://localhost:44303/stalker/users";
+
         public void initSearch()
         {
             search = "";
         }
-        public void startSearch()
+
+        public async void startSearch()
         {
-            Console.WriteLine(search);
-            oldSearch = search;
+            apiUrl += "?username=" + search;
+            Console.WriteLine(apiUrl);
+            person = await Http.GetFromJsonAsync<Person>(apiUrl);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Id);
             search = "";
         }
+
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Username { get; set; }
+            public string Photo { get; set; }
+        };
     }
 }
