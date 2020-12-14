ec54196fffa26447ccf1cbc31b6210431fa2a2ee
cosmin.aftanase <cosmin.aftanase@expertnetwork.ro>
2020-12-14 02:56:05 +0200

    Revert "Merge branch 'main' into StalkerMicroservice"
    This reverts commit 42bfbfb0ef9d9c9bc9d02dbaac0d70fac003c866, reversing
    changes made to 2a403f5d7261da31f0cb6a14bbf716dc932d9390.

---
+++ TweetMe.API/Stalker/Interfaces/ITwitterConnection.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace Stalker.Interfaces
+{
+    public interface ITwitterConnection
+    {
+        HttpClient GetTwitterClient();
+    }
+}
---
+++ TweetMe.API/Stalker/Views/FriendListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Stalker.Views
+{
+    public class FriendListViewModel
+    {
+        [JsonProperty("users")]
+        public List<UserViewModel> Users { get; set; }
+    }
+}
