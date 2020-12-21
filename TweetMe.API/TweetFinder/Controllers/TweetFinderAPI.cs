7f2da086e5ee8f6c4fddbf7afc41cc99aad9f84b
daniel-bicu <danielyoi1233@gmail.com>
2020-12-21 21:45:59 +0200

    TweetFinder microservice added

---
+++ TweetMe.API/TweetFinder/Controllers/TweetFinderAPI.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TweetFinder.Data;
+
+namespace TweetFinder.Controllers
+{
+    [Route("tweetfinder/")]
+    [ApiController]
+    public class TweetFinderAPI : ControllerBase
+    {
+        public DataContext _context { get; set; }
+        public TweetFinderAPI(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpPatch("{twitter_id}")]
+        public SearchedTweets UpdateSearchedTimes(int twitter_id)
+        {
+            var searchedTweet = _context.SearchedTweets.Select(tweet => tweet).Where(tweet=> tweet.TwitterId == twitter_id).FirstOrDefault();
+            
+            if(searchedTweet != null)
+            {
+                int nrOfSearched = searchedTweet.Searched;
+
+                searchedTweet.Update(nrOfSearched + 1);
+            }
+            else
+            {
+                int nrOfSearched = 1;
+                searchedTweet = new SearchedTweets()
+                {
+                    Searched = nrOfSearched,
+                    TwitterId = twitter_id
+                };
+
+                _context.SearchedTweets.Add(searchedTweet);
+                _context.SaveChanges();
+            }
+
+            return searchedTweet;
+        }
+    }
+}
---
+++ TweetMe.API/TweetFinder/Data/SearchedTweets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetFinder.Data
+{
+    public class SearchedTweets
+    {
+        public SearchedTweets()
+        {
+
+        }
+        public int ID { get; set; }
+        public long TwitterId { get; set; }
+        public int Searched { get; set; }
+
+        public void Update(int searched)
+        {
+            Searched = searched;
+        }
+
+    }
+}
